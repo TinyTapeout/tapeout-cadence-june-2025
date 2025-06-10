939405249cf0a713a1224c5f2ab9155634aee4a4
Uri Shaked <uri@urishaked.com>
2025-06-10 10:02:06 +0300

    feat: update TTCAD25a (commit 734c1d4e)

--- scripts/ttcad25a.py
+++ scripts/ttcad25a.py
@@ -13,9 +13,9 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMP_DIR = os.path.join(SCRIPT_DIR, "temp")
 FINAL_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, "../oas"))
 
-# GDS run: https://github.com/TinyTapeout/tinytapeout-cad-25a/actions/runs/15476860520
-# Precheck run: https://github.com/TinyTapeout/tinytapeout-cad-25a/actions/runs/15478341571
-commit_hash = "3ad64c0c"
+# GDS run: https://github.com/TinyTapeout/tinytapeout-cad-25a/actions/runs/15550104754
+# Precheck run: https://github.com/TinyTapeout/tinytapeout-cad-25a/actions/runs/15551439891
+commit_hash = "734c1d4e"
 
 top_cell_name = f"{SHUTTLE_ID}_{commit_hash}"
 temp_oas = os.path.join(TEMP_DIR, f"{top_cell_name}.oas")
